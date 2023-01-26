99f4eceed3caa584e616b593767299c56fd84ee9
SattuBaby <satyam0505007@outlook.com>
2023-01-26 21:29:13 +0530

    69

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function myFunction() {
     var dots = document.getElementById("check");
     var moreText = document.getElementById("more");
     var btn = document.getElementById("readMore");
-  
+    var langText = document.getElementById("languages");
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       btn.innerHTML = "Read More ↯"; 
       moreText.style.display = "none";
+      langText.style.display= "none"
     } else {
       dots.style.display = "none";
       btn.innerHTML = "Read Less ᐱ"; 
       moreText.style.display = "inline";
+      langText.style.display = "inline";
     }
   }

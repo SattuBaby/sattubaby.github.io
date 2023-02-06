6369ecd61b4d3a81648973972af46f852cf40d64
SattuBaby <satyam0505007@outlook.com>
2023-02-06 17:17:06 +0530

    Update

--- news.js
+++ news.js
@@ -1,10 +1,1 @@
-let news = {
-    "apiKey":"f81f8dc916074217ad401817b60e09e9",
-    fetchNews:function(country){
-        fetch(
-            "https://newsapi.org/v2/top-headlines?country="+country+"&apiKey="+this.apiKey
-        )
-        .then((Response)=>Response.json())
-        .then((data)=>this.diplayNews(data));
-    }
-}
+
--- script.js
+++ script.js
@@ -1,21 +1,61 @@
 const btn = document.querySelector('.btn');
-
 const text = document.querySelector('.more');
-
 const about = document.querySelector('.upper');
-
 about
     .addEventListener('click', e => {
       const current = e.target;
-
       const isReadMoreBtn = current.className.includes('btn');
-
       if (!isReadMoreBtn)
             return;
-
         const currentText = e.target.parentNode.querySelector('.more');
-
         currentText.classList.toggle('more--open');
-
         current.textContent = current.textContent.includes('Read More...') ? 'Read Less...' : 'Read More...';
     });
+let btc = {
+    "bitcoin":"bitcoin",
+    fetchBTC: function(){
+        fetch(
+            "https://api.coingecko.com/api/v3/simple/price?ids="+this.bitcoin+"&vs_currencies=usd&include_market_cap=false&include_24hr_vol=flase"
+        )
+        .then((Response)=>Response.json())
+        .then((data)=>this.displayPrice(data));
+    },
+    displayPrice : function(data){
+        const{usd} = data.bitcoin;
+        console.log(usd)
+        document.querySelector(".bitcoin").innerHTML = "BTC: "+usd+" $";
+    },
+}
+btc.fetchBTC()
+let ethereum = {
+    "ethereum":"ethereum",
+    fetchETH: function(){
+        fetch(
+            "https://api.coingecko.com/api/v3/simple/price?ids="+this.ethereum+"&vs_currencies=usd&include_market_cap=false&include_24hr_vol=flase"
+        )
+        .then((Response)=>Response.json())
+        .then((data)=>this.displayPrice(data));
+    },
+    displayPrice : function(data){
+        const{usd} = data.ethereum;
+        console.log(usd)
+        document.querySelector(".ethereum").innerHTML = "ETH: "+usd+" $";
+    },
+}
+ethereum.fetchETH()
+let doge = {
+    "dogecoin":"dogecoin",
+    fetchDOGE: function(){
+        fetch(
+            "https://api.coingecko.com/api/v3/simple/price?ids="+this.dogecoin+"&vs_currencies=usd&include_market_cap=false&include_24hr_vol=flase"
+        )
+        .then((Response)=>Response.json())
+        .then((data)=>this.displayPrice(data));
+    },
+    displayPrice : function(data){
+        const{usd} = data.dogecoin;
+        console.log(usd)
+        document.querySelector(".dogecoin").innerHTML = "DOGE: "+usd+" $";
+    },
+}
+doge.fetchDOGE()
